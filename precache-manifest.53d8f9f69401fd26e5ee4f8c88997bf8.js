2ed2f835e5da24da8ae34b37db56cb3a97eda990
Jackson Hall <jacksonhall@Jacksons-MacBook-Pro-2.local>
2019-08-09 17:17:36 -0400

    Updates

---
+++ precache-manifest.53d8f9f69401fd26e5ee4f8c88997bf8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab110959817bbc9a2d038989e4697f80",
+    "url": "/oversight/index.html"
+  },
+  {
+    "revision": "875ab0d5dc3d8e621ba7",
+    "url": "/oversight/static/css/main.f1690810.chunk.css"
+  },
+  {
+    "revision": "55123e49326c75a05d83",
+    "url": "/oversight/static/js/2.40e5a9e8.chunk.js"
+  },
+  {
+    "revision": "875ab0d5dc3d8e621ba7",
+    "url": "/oversight/static/js/main.84a7f8b5.chunk.js"
+  },
+  {
+    "revision": "873ca53726e07c71a40d",
+    "url": "/oversight/static/js/runtime~main.5730de76.js"
+  },
+  {
+    "revision": "ef8d9cd02dedff792adb913ceba799cf",
+    "url": "/oversight/static/media/add_engagement.ef8d9cd0.png"
+  },
+  {
+    "revision": "8fd0ec5d3ec0b46eaca37ae2f44e4bde",
+    "url": "/oversight/static/media/kids_ipad.8fd0ec5d.png"
+  },
+  {
+    "revision": "9c78e7bd2c95fa438c63dda4fec71b30",
+    "url": "/oversight/static/media/logo.9c78e7bd.png"
+  },
+  {
+    "revision": "a9ea49372cf8d765031acba5871e3187",
+    "url": "/oversight/static/media/message_swipe.a9ea4937.png"
+  },
+  {
+    "revision": "24628c6f7699e26803e78c612e313224",
+    "url": "/oversight/static/media/oversignal_over_silos.24628c6f.png"
+  },
+  {
+    "revision": "243097315c7213eadf2c154a33a3b92c",
+    "url": "/oversight/static/media/silos.24309731.png"
+  }
+]);
